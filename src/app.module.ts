48037f7c817d72c7a78ac7006faf7d83cbc0ce64
Mihailo Cvjetinovic <127780035+mihailocv@users.noreply.github.com>
2025-07-14 22:32:21 +0200

    Move database url to .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://admin:lozinka123@localhost:27017/?authSource=admin',
-    ),
-    UsersModule,
-    AuthModule,
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
     }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    AuthModule,
     UsersModule,
   ],
   controllers: [AppController],
