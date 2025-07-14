d205628822d20756cb9d1318a5106c8a25a7a12f
mihailocv <127780035+mihailocv@users.noreply.github.com>
2025-07-14 02:34:48 +0200

    Connect to frontend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Change in production to allow specific origins
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
